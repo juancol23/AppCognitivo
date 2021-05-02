605b370d2b3a81e4dc2025324319537ae37a93f4
DonValdemar <juan.colichon@vasslatam.com>
2021-05-01 20:13:40 -0500

    reporte de notas

--- app/src/main/java/com/valdemar/appcognitivo/ResultadoFinal.java
+++ app/src/main/java/com/valdemar/appcognitivo/ResultadoFinal.java
@@ -6,10 +6,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class ResultadoFinal extends AppCompatActivity {
 
     private SharedPreferences prefs = null;
     private TextView resultado_final;
+    int buenas = 0;
+    int malas = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,29 @@ public class ResultadoFinal extends AppCompatActivity {
 
         prefs = getSharedPreferences("com.valdemar.appcognitivo", MODE_PRIVATE);
 
-        resultado_final.setText(prefs.getString("prefs_puntaje",""));
+
+        String listaPuntajeOld = prefs.getString("prefs_puntaje","").replace("[", "");
+        String listaPuntajeNew = listaPuntajeOld.replace("]", "");
+
+        String listaCalificacion [] = listaPuntajeNew.split(",");
+        for (int i = 0; i<10; i++) {
+            if (listaCalificacion[i].trim().equalsIgnoreCase("true")) {
+                buenas = buenas + 1;
+            } else {
+                malas = malas + 1;
+            }
+            if( i == 9){
+                System.out.println(buenas);
+                System.out.println(malas);
+                resultado_final.setText(prefs.getString("prefs_notificacion","")+" Aprobaste "+buenas+" de 10");
+            }
+            if( i == 10){
+
+                System.out.println(buenas);
+                System.out.println(malas);
+                resultado_final.setText(prefs.getString("prefs_notificacion","")+" Aprobaste "+malas+" de 10");
+            }
+        }
 
 
     }
